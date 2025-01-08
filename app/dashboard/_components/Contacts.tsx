4e4c55582057a01af61eee4cd86b19a688170861
Wisdom Ojimah <vindicatedwisdom@gmail.com>
2025-01-08 16:55:49 +0100

    style: minor style changes

--- app/dashboard/_components/Contacts.tsx
+++ app/dashboard/_components/Contacts.tsx
@@ -6,54 +6,52 @@ import { CircleDotIcon } from "lucide-react";
 const Contacts = () => {
 	return (
 		<section className="grid gap-4 md:grid-cols-2">
-			<div className="rounded-lg bg-brand-white text-brand-black border border-gray-300 grid">
-				<div className="text-center p-4 space-y-2 grid place-content-center">
-					<Image
-						className="size-16 rounded-full mx-auto"
-						src={UserImage}
-						alt="Wwisdom"
-					/>
+			<div className="rounded-lg bg-brand-white text-brand-black border border-gray-300 flex gap-2 items-center">
+				<Image
+					className="w-1/3 h-full rounded-l-lg"
+					src={UserImage}
+					alt="Wwisdom"
+				/>
 
-					<p className="text-sm max-w-[80%] mx-auto">
+				<div className="py-4 pr-4 grid gap-4">
+					<p className="text-sm">
 						List your profile
 					</p>
-				</div>
 
-				<Link
-					className="rounded-b-lg bg-lime-500 text-white hover:bg-lime-600 transition-colors duration-300 ease-in-out p-3 text-center w-full"
-					href="/dashboard/listing/add"
-				>
-					Get listed
-				</Link>
+					<Link
+						className="text-lime-500"
+						href="/dashboard/listing/add"
+					>
+						Get listed
+					</Link>
+				</div>
 			</div>
 
 			{Array.from({ length: 5 }).map((_, index) => (
 				<div
-					className="rounded-lg bg-brand-white text-brand-black border border-gray-300 grid"
+					className="rounded-lg bg-brand-white text-brand-black border border-gray-300 flex gap-2 items-center"
 					key={index}
 				>
-					<div className="text-center p-4 space-y-2 grid place-content-center">
-						<Image
-							className="size-16 rounded-full mx-auto"
-							src={UserImage}
-							alt="Wwisdom"
-						/>
+					<Image
+						className="w-1/3 h-full rounded-l-lg"
+						src={UserImage}
+						alt="Wwisdom"
+					/>
 
-						<p className="text-sm max-w-[80%] mx-auto">
-							OluwaTosinFaith
-						</p>
+					<div className="py-4 pr-4 grid gap-4">
+						<p className="text-sm">OluwaTosinFaith</p>
+
+						<button
+							className="text-center text-sm inline-flex items-center gap-2 text-lime-500 z-10 font-medium"
+							type="button"
+						>
+							Add +5{" "}
+							<CircleDotIcon
+								size={18}
+								strokeWidth={1.5}
+							/>
+						</button>
 					</div>
-
-					<button
-						className="text-center text-sm max-w-[60%] my-4 mx-auto inline-flex items-center gap-2 text-lime-500 z-10 font-medium"
-						type="button"
-					>
-						Add +5{" "}
-						<CircleDotIcon
-							size={18}
-							strokeWidth={1.5}
-						/>
-					</button>
 				</div>
 			))}
 		</section>
