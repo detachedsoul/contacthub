b47eaab29ab5d2fe2d11c5dbb79bff3a26118d6a
musa <musababalola1998@gmail.com>
2025-03-07 15:52:23 +0100

    merge

--- pages/api/webhook/payvessel.ts
+++ pages/api/webhook/payvessel.ts
@@ -17,7 +17,7 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   const  whitelist = ["162.246.254.36", "3.255.23.38"]
   
   if ( !whitelist.includes(ipAddress)) {
-    return res.status(405).json({ message: `Uknown Ip Address` });
+    return res.status(405).json({ message: `Uknown Ip Address ${ipAddress} ${req.headers}` });
   }
 
   const payload: any = req.body;
